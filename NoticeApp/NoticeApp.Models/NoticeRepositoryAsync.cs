3738595455c33c3bd6c2312ed168b4a6406a604a
BOOK-LV5E6VJEQB\rlady <youngwoo>
2024-02-07 22:18:06 +0900

    02_04_Repository 클래스에 생성자로 DbContext 주입 후 AddAsync 메서드의 기본 코드 작성

--- NoticeApp/NoticeApp.Models/NoticeRepositoryAsync.cs
+++ NoticeApp/NoticeApp.Models/NoticeRepositoryAsync.cs
@@ -1,4 +1,5 @@
 ﻿using Dul.Domain.Common;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -8,43 +9,73 @@ using System.Threading.Tasks;
 namespace NoticeApp.Models
 {
     /// <summary>
-    /// [6] Repository Class
+    /// [6] Repository Class: ADO.NET or Dapper or Entity Framework Core
     /// </summary>
     public class NoticeRepositoryAsync : INoticeRepositoryAsync
     {
-        public Task<Notice> AddAsync(Notice model)
+        private readonly NoticeAppDbContext _context;
+        private readonly ILogger _logger;
+
+        // 생성자
+        public NoticeRepositoryAsync(NoticeAppDbContext context, ILoggerFactory loggerFactory)
         {
-            throw new NotImplementedException();
+            this._context = context;
+            this._logger = loggerFactory.CreateLogger(nameof(NoticeRepositoryAsync));
         }
 
-        public Task<bool> DeleteAsync(int id)
+        // 입력
+        public async Task<Notice> AddAsync(Notice model)
         {
-            throw new NotImplementedException();
+            _context.Notices.Add(model);
+            
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"에러 발생({nameof(AddAsync)}) : {ex.Message}");
+            }
+
+            return model;
         }
 
-        public Task<bool> EditAsync(Notice model)
-        {
-            throw new NotImplementedException();
-        }
-
+        // 출력
         public Task<List<Notice>> GetAllAsync()
         {
             throw new NotImplementedException();
         }
 
+        // 상세
+        public Task<Notice> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 수정
+        public Task<bool> EditAsync(Notice model)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 삭제
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // 페이징
         public Task<PagingResult<Notice>> GetAllAsync(int skip, int take)
         {
             throw new NotImplementedException();
         }
 
+        // 부모
         public Task<PagingResult<Notice>> GetAllByParentIdAsync(int pageIndex, int pageSize, int parentId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Notice> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+    
     }
 }
