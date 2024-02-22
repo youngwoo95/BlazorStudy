cb2645c338702aa90f6cd52792c54f0505f838b6
kyw <rladyddn258@s-tec.co.kr>
2024-02-22 15:56:21 +0900

    EditForm 사용예제

---
+++ BlazorManager/Data/Models/ExampleModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorManager.Data.Models
+{
+    public class ExampleModel
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 제목
+        /// </summary>
+        [Required(ErrorMessage = "제목을 입력해주세요")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 내용
+        /// </summary>
+        [Required(ErrorMessage = "내용을 입력해주세요")]
+        public string Content { get; set; }
+
+    }
+}
