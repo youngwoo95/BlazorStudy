86936062d286a3e2ac396221914fd966f9d18343
DESKTOP-551ENR9\kyw <rladyddn258@gmail.com>
2024-03-05 23:20:05 +0900

    6. 뷰 생성해보기

--- ASPNETExample/ASP.NET_MVC/Source/Demo01/Demo01/Controllers/HomeController.cs
+++ ASPNETExample/ASP.NET_MVC/Source/Demo01/Demo01/Controllers/HomeController.cs
@@ -12,11 +12,34 @@ namespace Demo01.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        // Home, / 모두다 Index()로 라우팅처리된다.
-        public string Index()
+
+        // 액션 메서드에서 ViewResult외에도 RedirectResult 또는 HttpUnauthorizedResult라는 Result 객체를 사용할 수 있다.
+        // 이와 같은 객체들을 Action Result라고 한다. 이 개체들은 ActionResult 클래스에서 파생된 객체들이다.
+
+        // RedirectResult : 브라우저가 다른 URL로 재요청 하게된다.
+        // HttpUnauthorizedResult : 사용자 로그인을 하도록 하게 한다.
+        /*
+        public ViewResult Index()
         {
-            return "Hello World!!!";
+            // ViewResult 객체 리턴
+            return View(); // View() 메서드는 ViewResult 객체를 생성하는 메서드
         }
+        */
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Insa = hour < 12 ?  "좋은 아침" : "즐거운 오후";
+
+            return View();
+        }
+
+
+        // Home, / 모두다 Index()로 라우팅처리된다.
+        //public string Index()
+        //{
+        //    return "Hello World!!!";
+        //}
 
 
         /*
